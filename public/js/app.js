d0991d0399f7f107d73284c3c4a8e1a7c6c2817d
Sudarshan Reddy <Sudarshan.Reddy+ghub@blueyonder.com>
2025-06-27 19:42:53 +0530

    changes to make heroku work

--- public/js/app.js
+++ public/js/app.js
@@ -9,7 +9,8 @@ weatherForm.addEventListener('submit', (e) => {
     const location = search.value
     e.preventDefault()
 
-    const url = 'http://localhost:3000/weather?address=' + location
+    // const url = 'http://localhost:3000/weather?address=' + location
+    const url = '/weather?address=' + location
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
